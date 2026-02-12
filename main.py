a89f5cc6bf812fdf61bde692aaf7e13656fa5201
Dadowatt <dadowatt92@gmail.com>
2026-02-12 15:47:12 +0100

    implementation du menu console

--- main.py
+++ main.py
@@ -23,3 +23,23 @@ try:
     print("Connexion réussie à la base de données MySQL")
 except mysql.connector.Error as e:
     print(f"Erreur de connexion : {e}")
+
+#menu pricipal
+def menu_principal():
+    while True:
+        print("Menu Principal")
+        print("1. Se connecter")
+        print("2. S'inscrire")
+        print("3. Quitter")
+
+        choix = input("Veuillez choisir une option : ")
+        if choix == "1":
+            print("se connecter")
+        elif choix == "2":
+            print("s'inscrire")
+        elif choix == "3":
+            print("Au revoir!")
+            exit()
+        else:
+            print("Option invalide. Veuillez réessayer.")
+menu_principal()
